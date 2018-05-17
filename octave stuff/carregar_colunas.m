82e1faeb4e2e10f2f0105520a528682617c8093f
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2018-05-17 15:32:30 -0300

    Update

--- octave stuff/carregar_colunas.m
+++ octave stuff/carregar_colunas.m
@@ -2,8 +2,8 @@ function colunas = carregar_colunas(arquivo_csv)
   printf("Carregando colunas da base de dados.\n", i);
   
   num_colunas = 171;
-	colunas = cell(1, num_colunas);
-	[colunas{:}] = textread(arquivo_csv, repmat('%s', [1, num_colunas]), 'Delimiter', ',');
+	colunas_texto = cell(1, num_colunas);
+	[colunas_texto{:}] = textread(arquivo_csv, repmat('%s', [1, num_colunas]), 'Delimiter', ',');
   
-  save("-binary", "carregar_colunas.mat", "colunas");
+  save("-binary", "carregar_colunas.mat", "colunas_texto");
 end
