a27b53a2cdbe50efdaf0c37ea7781a30a53d5f0b
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2018-05-19 02:43:51 -0300

    Update

--- octave stuff/processamento/preprocessamento.m
+++ octave stuff/processamento/preprocessamento.m
@@ -1,9 +1,9 @@
-more off;
-
-colunas_texto = carregar_colunas('aps_failure_training_set.csv');
-[X, y] = converter_colunas(eval("colunas_texto", "[]"));
-[faltantes_linhas, faltantes_colunas] = analisar_faltantes(eval("X", "[]"));
-[X, y, colunas_removidas] = remover_dados(eval("X", "[]"), eval("y", "[]"), ...
-  eval("faltantes_linhas", "[]"), eval("faltantes_colunas", "[]"));  
-X = preencher_faltantes(eval("X", "[]"));
-X = normalizar_dados(eval("X", "[]"));
+function [X, y] = preprocessamento(arquivo_csv)
+  #colunas_texto = carregar_colunas(arquivo_csv);
+  #[X, y] = converter_colunas(eval("colunas_texto", "[]"));
+  [faltantes_linhas, faltantes_colunas] = analisar_faltantes(eval("X", "[]"));
+  #[X, y, colunas_removidas] = remover_dados(eval("X", "[]"), eval("y", "[]"), ...
+  #  eval("faltantes_linhas", "[]"), eval("faltantes_colunas", "[]"));  
+  #X = preencher_faltantes(eval("X", "[]"));
+  #X = normalizar_dados(eval("X", "[]"));
+end  
