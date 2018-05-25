ec0dd9bcc0e18288555a8f4cccff52b8a86cc609
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2018-05-24 23:11:18 -0300

    Update

--- octave stuff/preprocessamento/preprocessamento.m
+++ octave stuff/preprocessamento/preprocessamento.m
@@ -1,7 +1,7 @@
 function [X, y] = preprocessamento(arquivo_csv)
-  #colunas_texto = carregar_colunas(arquivo_csv);
-  #[X, y] = converter_colunas(eval("colunas_texto", "[]"));
-  [X, y] = remover_dados(eval("X", "[]"), eval("y", "[]"), 10, 20);
-  X = preencher_faltantes(eval("X", "[]"));
-  X = normalizar_dados(eval("X", "[]"));
+  %colunas_texto = carregar_colunas(arquivo_csv);
+  %[X, y] = converter_colunas(eval('colunas_texto', '[]'));
+  [X, y] = remover_dados(eval('X', '[]'), eval('y', '[]'), 10, 20);
+  X = preencher_faltantes(eval('X', '[]'));
+  X = normalizar_dados(eval('X', '[]'));
 end
