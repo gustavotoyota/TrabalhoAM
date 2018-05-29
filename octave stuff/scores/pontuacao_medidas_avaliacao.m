c62298d593f6e48b54bb700e496c649a16e1520e
Pedro Reis Pires <pedro.pires@dcomp.sor.ufscar.br>
2018-05-29 13:04:31 -0300

    Create pontuacao_medidas_avaliacao.m

---
+++ octave stuff/scores/pontuacao_medidas_avaliacao.m
@@ -0,0 +1,27 @@
+% Calcula diversas medidas de avaliacao sobre a predicao
+
+% ENTRADA
+%      y = [Mx1] rotulos reais
+%   pred = [Mx1] rotulos preditos pelo classificador
+
+% SAIDA
+%   acc = [1x1] acuracia
+%  prec = [1x1] precisao media
+%   rec = [1x1] revocacao media
+%    f1 = [1x1] f-medida
+
+function [acc, prec, rec, f1] = pontuacao_medidas_avaliacao(y, pred)
+
+	% Calcula a matriz de confusao
+	tn = sum((pred==0)&(y==0));
+	fn = sum((pred==0)&(y==1));
+	fp = sum((pred==1)&(y==0));
+	tp = sum((pred==1)&(y==1));
+
+	% Calcula as medidas de avaliacao
+	acc = (tp + tn)/(tp + tn + fp + fn);
+	prec = tp/(tp+fp);
+	rec = tp/(tp+fn);
+	f1 = 2*((prec*rec)/(prec+rec));
+
+end
