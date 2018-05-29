5f741f748394f94d31debc60340106a0d4ef601b
Pedro Reis Pires <pedro.pires@dcomp.sor.ufscar.br>
2018-05-29 13:05:17 -0300

    Update regressao_logistica_treinar.m

--- octave stuff/metodos/regressao_logistica/regressao_logistica_treinar.m
+++ octave stuff/metodos/regressao_logistica/regressao_logistica_treinar.m
@@ -11,26 +11,31 @@
 %   theta = [Nx1] thetas treinados
 
 function [J, theta] = regressao_logistica_treinar(X, y, alpha, lambda, max_iter)
-% Inicializa o historico da funcao custo
-J = zeros(max_iter,1);
+	% Define funcao sigmoid
+	sigmoid = @(z) 1 ./ (1 + exp(-z));
 
-% Armazena o numero de amostras
-m = length(y);
+	% Inicializa o historico da funcao custo
+	J = zeros(max_iter,1);
 
-% Adiciona coluna de vies
-X = [ones(size(X)(1),1) X];
+	% Armazena o numero de amostras
+	m = length(y);
 
-% Inicializa theta randomicamente
-theta = rand(size(X)(2),1);
+	% Adiciona coluna de vies
+	X = [ones(size(X)(1),1) X];
 
-for iter = 1:max_iter  
-  theta = theta - alpha*((sum((sigmoide(X*theta)-y).*X)/m)' + (lambda/m)*theta);
-  theta(1) = theta(1) - alpha*(sum((sigmoide(X*theta)-y).*X(:,1))/m);
+	% Inicializa theta randomicamente
+	theta = rand(size(X)(2),1);
+
+	for iter = 1:max_iter  
+	  theta = theta - alpha*((sum((sigmoid(X*theta)-y).*X)/m)' + (lambda/m)*theta);
+	  theta(1) = theta(1) - alpha*(sum((sigmoid(X*theta)-y).*X(:,1))/m);
+       	    
+	  J(iter) = sum(-y.*log(sigmoid(X*theta)+1e-15)-(1-y).*log(1-sigmoid(X*theta)+1e-15))/m + lambda/(2*m) * sum(theta(2:end).^2);	     
+    fprintf("%f\n", J(iter));
     
-  J(iter) = sum(-y.*log(sigmoide(X*theta))-(1-y).*log(1-sigmoide(X*theta)))/m + lambda/(2*m) * sum(theta(2:end).^2);
-  
-  if iter > 1 && J(iter-1) == J(iter)
-    J = J(1:end-iter,:);
-    break
-  end
+	  if iter > 1 && J(iter-1) == J(iter)
+	    J = J(1:end-iter,:);
+	    break
+	  end
+	end
 end
