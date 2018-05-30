bd52a480c275a4b5414052182b42e279e4c4c2fd
matheus jesus <matheus.s.jesus14@gmail.com>
2018-05-30 09:56:06 -0300

    update

--- octave stuff/metodos/svm/SVM.m
+++ octave stuff/metodos/svm/SVM.m
@@ -1,9 +1,28 @@
-%carrega os arquivos de teste e treino gerados pelo script tm python
-[base_treino_label, base_treino_data] = libsvmread('base_treino.lbsvm');
-[base_teste_label, base_teste_data] = libsvmread('base_teste.lbsvm');
+% Método SVM
+
+% ENTRADA
+%   X = [MxN] amostras de treinamento
+%   Y = [Mx1] rotulos das amostras de treinamento
+%   A = [PXN] amostras de teste
+%   B = [PX1] rotulos das amostras de teste
+
+% SAIDA
+%   predicted_labels = os rotulos dados para os modelos de teste
+%   accuracy = acuracia do modelo aplicado nos dados de teste
+
+function [predicted_labels, accuracy] = svm(X, Y, A, B) 
+%carrega os arquivos de teste e treino (se necessario? nao sei como vai rodar tudo no fim)
+%[Y, X] = libsvmread('base_treino.lbsvm');
+%[B, A] = libsvmread('base_teste.lbsvm');
 
 %treina com os dados e gera um modelo
-model = svmtrain(base_treino_label, base_treino_data, '-c 1 -g 0.07');
+model = svmtrain(Y, X);
 
-%testa o modelo com os dados de teste e retorna um vetor das predicoes, acuracia
-[predicted_labels, accuracy, dec_values] = svmpredict(base_teste_label, base_teste_data, model);
+%testa o modelo com os dados de teste e retorna um vetor das predicoes e acuracia
+[predicted_labels, accuracy, dec_values] = svmpredict(B, A, model);
+
+%para escrever os dados em formato libsvm:
+%libsvmwrite('base_treino.lbsvm', Y, X);
+%libsvmwrite('base_teste.lbsvm', B, A);
+
+end
