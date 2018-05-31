492905f70e7e76e6b630e4589090551eb8fc7b96
Pedro Reis Pires <pedro.pires@dcomp.sor.ufscar.br>
2018-05-30 21:09:40 -0300

    Update preprocessamento.m

--- octave stuff/preprocessamento/preprocessamento.m
+++ octave stuff/preprocessamento/preprocessamento.m
@@ -1,7 +1,9 @@
 function [X, y] = preprocessamento(arquivo_csv)
   %colunas_texto = carregar_colunas(arquivo_csv);
   %[X, y] = converter_colunas(eval('colunas_texto', '[]'));
-  [X, y] = remover_dados(eval('X', '[]'), eval('y', '[]'), 10, 20);
+  X = remover_outliers(eval('X', '[]'));
+  [X, y] = remover_dados(eval('X', '[]'), eval('y', '[]'), 10, 50);
+  %[X, y] = balancear_classes(eval('X','[]'), eval('y','[]'));
   X = preencher_faltantes(eval('X', '[]'));
   X = normalizar_dados(eval('X', '[]'));
 end
