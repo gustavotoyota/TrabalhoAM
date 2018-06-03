bb66d06b0c8b78d88e49a1fb5e06e0d96fa0146e
Pedro Reis Pires <pedro.pires@dcomp.sor.ufscar.br>
2018-06-03 10:31:36 -0300

    Add files via upload

--- octave stuff/metodos/rede_neural/rede_neural_prever.m
+++ octave stuff/metodos/rede_neural/rede_neural_prever.m
@@ -1,18 +1,33 @@
-function outputs_output_layer = rede_neural_prever(rede_neural, X)
+% Método Rede Neural Artificial com ReLu e Softmax para para predizer amostras
+
+% ENTRADA
+%     x = [MxN] amostras a serem rotuladas
+%   clf = estrutura contendo:
+%       historico = [max_iterx1] historico dos valores da funcao custo 
+%           peso1 = [N+1xtam_hidden_layer] pesos entre camada de entrada e intermediaria
+%           peso2 = [tam_hidden_layer+1x1] pesos entra camada intermediaria e de saida
+
+% SAIDA
+%   pred = [Mx1] previsao das amostras
+
+function pred = rede_neural_prever(x, clf)
   % Funcoes de ativacao
   leaky_relu = @(input) (input < 0) .* 0.01 .* input + (input >= 0) .* input;
   softmax = @(input) exp(input - max(input, [], 2)) ./ sum(exp(input - max(input, [], 2)), 2);
   
   % Adicionar bias ao X
-  X(:, end + 1) = 1;
+  x(:, end + 1) = 1;
   
   % Feedforward
   % - Hidden layer
-  inputs_hidden_layer = X * rede_neural.pesos1;
+  inputs_hidden_layer = x * clf.pesos1;
   outputs_hidden_layer = leaky_relu(inputs_hidden_layer);
   
   % - Output layer
   outputs_hidden_layer(:, end + 1) = 1;
-  inputs_output_layer = outputs_hidden_layer * rede_neural.pesos2;
+  inputs_output_layer = outputs_hidden_layer * clf.pesos2;
   outputs_output_layer = softmax(inputs_output_layer);
+  
+  % Realiza a predicao
+  pred = round(outputs_output_layer(:,2));
 endfunction
--- octave stuff/metodos/rede_neural/rede_neural_treinar.m
+++ octave stuff/metodos/rede_neural/rede_neural_treinar.m
@@ -1,4 +1,21 @@
-function rede_neural = rede_neural_treinar(X, y, opcoes)
+% Método Rede Neural Artificial com ReLu e Softmax para aprender os pesos da rede
+
+% ENTRADA
+%          X = [MxN] amostras de treinamento
+%          y = [Mx1] rotulos das amostras de treinamento
+%     opcoes = estrutura contendo:
+%        tam_hidden_layer = [1x1] numero de neuronios da camada intermediaria
+%                max_iter = [1x1] numero de iteracoes da rede
+%        taxa_aprendizado = [1x1] taxa de aprendizado
+
+% SAIDA
+%   clf = estrutura contendo:
+%       historico = [max_iterx1] historico dos valores da funcao custo 
+%           peso1 = [N+1xtam_hidden_layer] pesos entre camada de entrada e intermediaria
+%           peso2 = [tam_hidden_layer+1x1] pesos entra camada intermediaria e de saida
+
+
+function clf = rede_neural_treinar(X, y, opcoes)
   % Funcoes de ativacao
   leaky_relu = @(input) (input < 0) .* 0.01 .* input + (input >= 0) .* input;
   deriv_leaky_relu = @(input) (input < 0) .* 0.01 + (input >= 0) .* 1;
@@ -12,7 +29,7 @@ function rede_neural = rede_neural_treinar(X, y, opcoes)
   
   % Carregar opcoes usando a funcao eval(try, catch)
   tam_hidden_layer = eval('opcoes.tam_hidden_layer', '10');
-  num_iteracoes = eval('opcoes.num_iteracoes', '100');
+  max_iter = eval('opcoes.max_iter', '100');
   taxa_aprendizado = eval('opcoes.taxa_aprendizado', '0.01');
   
   % Modificar y para classificacao com softmax
@@ -33,28 +50,32 @@ function rede_neural = rede_neural_treinar(X, y, opcoes)
   qtd_y_neg = sum(pos_y_neg); % Quantidade de amostras de classe neg
   qtd_y_pos = sum(pos_y_pos); % Quantidade de amostras de classe pos
   
+  % Inicializa historico de custos
+  clf.historico = zeros(max_iter, 1);
+  
   % Inicializar aleatoriamente entre -1 e 1
-  rede_neural.pesos1 = rand(tam_input_layer + 1, tam_hidden_layer) .* 2 - 1;
-  rede_neural.pesos2 = rand(tam_hidden_layer + 1, tam_output_layer) .* 2 - 1;
+  clf.pesos1 = rand(tam_input_layer + 1, tam_hidden_layer) .* 2 - 1;
+  clf.pesos2 = rand(tam_hidden_layer + 1, tam_output_layer) .* 2 - 1;
   
   % Aplicar redistribuicao dos pesos
-  rede_neural.pesos1 *= sqrt(2 / (tam_input_layer + tam_hidden_layer));
-  rede_neural.pesos2 *= sqrt(2 / (tam_hidden_layer + tam_output_layer));
+  clf.pesos1 *= sqrt(2 / (tam_input_layer + tam_hidden_layer));
+  clf.pesos2 *= sqrt(2 / (tam_hidden_layer + tam_output_layer));
   
-  for i = 1 : num_iteracoes
+  for i = 1 : max_iter
     % Feedforward
     % - Hidden layer
-    inputs_hidden_layer = X * rede_neural.pesos1; % Multiplicar pelos pesos
+    inputs_hidden_layer = X * clf.pesos1; % Multiplicar pelos pesos
     outputs_hidden_layer = leaky_relu(inputs_hidden_layer); % Aplicar leaky relu aos inputs
     
     % - Output layer
     outputs_hidden_layer(:, end + 1) = 1; % Adicionar bias aos outputs do layer anterior
-    inputs_output_layer = outputs_hidden_layer * rede_neural.pesos2; % Multiplicar pelos pesos
+    inputs_output_layer = outputs_hidden_layer * clf.pesos2; % Multiplicar pelos pesos
     outputs_output_layer = softmax(inputs_output_layer); % Aplicar softmax aos inputs
     
     % Calcular custo total
     custo_total = cross_entropy(outputs_output_layer, y);
-    fprintf("Custo total: %f.\n", custo_total);
+    clf.historico(i) = custo_total;
+    %fprintf("Custo total: %f.\n", custo_total);
     
     % Backpropagation
     % - Output layer
@@ -69,7 +90,7 @@ function rede_neural = rede_neural_treinar(X, y, opcoes)
     
     % - Hidden layer
     % --- Calcular as derivadas parciais dos custos ate os pesos 1
-    deriv_custos_outputs = deriv_custos_inputs * rede_neural.pesos2(1 : end - 1, :)';
+    deriv_custos_outputs = deriv_custos_inputs * clf.pesos2(1 : end - 1, :)';
     deriv_outputs_inputs = deriv_leaky_relu(inputs_hidden_layer);
     deriv_custos_inputs = deriv_custos_outputs .* deriv_outputs_inputs;
     deriv_inputs_pesos = X;
@@ -80,7 +101,7 @@ function rede_neural = rede_neural_treinar(X, y, opcoes)
     grad_medio_pesos1 = (grad_total_pesos1_neg ./ qtd_y_neg + grad_total_pesos1_pos ./ qtd_y_pos) ./ 2;
       
     % - Aplicar o negativo do gradiente aos pesos, controlado pela taxa de aprendizado
-    rede_neural.pesos2 -= grad_medio_pesos2 .* taxa_aprendizado;
-    rede_neural.pesos1 -= grad_medio_pesos1 .* taxa_aprendizado;
+    clf.pesos2 -= grad_medio_pesos2 .* taxa_aprendizado;
+    clf.pesos1 -= grad_medio_pesos1 .* taxa_aprendizado;
   end
 endfunction
